00fe3df0403c8424c2863e82c932e0b0459146cc
derKosi <mathias.kosinski@gmail.com>
2020-10-11 17:22:47 +0000

    Updated .tt

--- Dalyan.Domain/Logger/Log.cs
+++ Dalyan.Domain/Logger/Log.cs
@@ -11,69 +11,71 @@ using Dalyan.Entities.Helper.IO;
 
 namespace Dalyan.Domain.Logger
 {
-    public class Log : ILog
+  public class Log : ILog
+  {
+    private readonly DbEntities _db;
+    public Log()
     {
-        private readonly DbEntities _db;
-        public Log()
-        {
-            _db = new DbEntities();
-        }
-
-        public void Logger(LogType logtype, LogLevel loglevel, Exception ex, String message)
-        {
-            WriteLog(logtype, loglevel, ex, message);
-        }
-
-        public void Logger(LogType logtype, LogLevel loglevel, Exception ex, String message, CurrentUser currentUser)
-        {
-            WriteLog(logtype, loglevel, ex, message, currentUser);
-        }
-        private void WriteLog(LogType logtype, LogLevel loglevel, Exception ex, String message, CurrentUser currentUser)
-        {
-            try
-            {
-                Dalyan.Db.LogDatabase log = new Dalyan.Db.LogDatabase();
-                log.Comment = message;
-                log.ExceptionString = ex.ToString();
-                log.LogDate = DateTime.Now;
-                log.LogLevel = loglevel.ToString();
-                log.LogType = logtype.ToString();
-                if (currentUser != null) log.UserId = currentUser.UserID;
-                log.IsDeleted = false;
-                _db.LogDatabase.Add(log);
-                _db.SaveChanges();
-            }
-            catch (Exception exLog)
-            {
-                WriteLogToFile(logtype, loglevel, ex, message, exLog);
-            }
-        }
-
-        private void WriteLog(LogType logtype, LogLevel loglevel, Exception ex, String message)
-        {
-            try
-            {
-                Dalyan.Db.LogDatabase log = new Dalyan.Db.LogDatabase();
-                log.Comment = message;
-                log.ExceptionString = ex.ToString();
-                log.LogDate = DateTime.Now;
-                log.LogLevel = loglevel.ToString();
-                log.LogType = logtype.ToString();
-                log.IsDeleted = false;
-                _db.LogDatabase.Add(log);
-                _db.SaveChanges();
-            }
-            catch (Exception exLog)
-            {
-                WriteLogToFile(logtype, loglevel, ex, message, exLog);
-            }
-        }
-
-        private void WriteLogToFile(LogType logtype, LogLevel loglevel, Exception ex, String message, Exception exlog)
-        {
-            string content = "Log \r\n" + DateTime.Now + "\r\n" + loglevel.ToString() + "\r\n" + logtype.ToString() + "\r\n Exception:" + ex.ToString() + "\r\n message:" + message + "\r\n Extra Log :" + exlog.ToString();
-            string filename = ConfigurationManager.AppSettings["logPath"] + DateTime.Now.Ticks.ToString() + ".txt";
-            FileHelper.WriteFile(content, filename);
-        }
+      _db = new DbEntities();
     }
+
+    public void Logger(LogType logtype, LogLevel loglevel, Exception ex, String message)
+    {
+      WriteLog(logtype, loglevel, ex, message);
+    }
+
+    public void Logger(LogType logtype, LogLevel loglevel, Exception ex, String message, CurrentUser currentUser)
+    {
+      WriteLog(logtype, loglevel, ex, message, currentUser);
+    }
+    private void WriteLog(LogType logtype, LogLevel loglevel, Exception ex, String message, CurrentUser currentUser)
+    {
+      try
+      {
+        Dalyan.Db.LogDatabase log = new Dalyan.Db.LogDatabase();
+        log.Comment = message;
+        log.ExceptionString = ex.ToString();
+        log.LogDate = DateTime.Now;
+        log.LogLevel = loglevel.ToString();
+        log.LogType = logtype.ToString();
+        if (currentUser != null) log.UserId = currentUser.UserID;
+        log.IsDeleted = false;
+        _db.LogDatabase.Add(log);
+        _db.SaveChanges();
+      }
+      catch (Exception exLog)
+      {
+        WriteLogToFile(logtype, loglevel, ex, message, exLog);
+      }
+    }
+
+    private void WriteLog(LogType logtype, LogLevel loglevel, Exception ex, String message)
+    {
+      try
+      {
+        Dalyan.Db.LogDatabase log = new Dalyan.Db.LogDatabase();
+        log.Comment = message;
+        log.ExceptionString = ex.ToString();
+        log.LogDate = DateTime.Now;
+        log.LogLevel = loglevel.ToString();
+        log.LogType = logtype.ToString();
+        log.IsDeleted = false;
+        _db.LogDatabase.Add(log);
+        _db.SaveChanges();
+      }
+      catch (Exception exLog)
+      {
+        WriteLogToFile(logtype, loglevel, ex, message, exLog);
+      }
+    }
+
+    private void WriteLogToFile(LogType logtype, LogLevel loglevel, Exception ex, String message, Exception exlog)
+    {
+      string content = "Log \r\n" + DateTime.Now + "\r\n" + loglevel.ToString() + "\r\n" + logtype.ToString() + "\r\n Exception:" + ex.ToString() + "\r\n message:" + message + "\r\n Extra Log :" + exlog.ToString();
+      string relativePath = ConfigurationManager.AppSettings["logPath"];
+      string combinedPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath);
+      string filename = combinedPath + DateTime.Now.Ticks.ToString() + ".txt";
+      FileHelper.WriteFile(content, filename);
+    }
+  }
 }
